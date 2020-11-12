193296c70221fbd5542ddbfc02efd24bfc578275
krharum <bc05aab8d40e1cec7718d2efaef17f0817190a61>
2020-11-12 13:25:29 +0100

    Lagt til vault token II

--- dolly-backend-app-nais/src/main/java/no/nav/dolly/ApplicationStarter.java
+++ dolly-backend-app-nais/src/main/java/no/nav/dolly/ApplicationStarter.java
@@ -9,7 +9,7 @@ public class ApplicationStarter extends SpringBootServletInitializer {
     public static void main(String[] args) {
 
         Map<String, Object> properties = PropertyReader.builder()
-                .readSecret("SPRING_CLOUD_VAULT_TOKEN", "/var/run/secrets/nais.io/vault/vault_token")
+                .readSecret("SPRING_CLOUD_VAULT_TOKEN", "/var/run/secrets/nais.io/vault")
                 .readSecret("ORACLE_DATASOURCE_USERNAME", "/var/run/secrets/nais.io/db/username")
                 .readSecret("ORACLE_DATASOURCE_PASSWORD", "/var/run/secrets/nais.io/db/password")
                 .readSecret("ORACLE_DATASOURCE_URL", "/var/run/secrets/nais.io/dbPath/jdbc_url")
