8396a1a1f63f860b8095a0805ae7915d02dc0e59
stigus <stian.gustavsson@nav.no>
2020-12-22 09:50:07 +0100

    Bedre dokumentasjon og feilhåndtering på OrganisasjonController

--- dolly-backend-app/src/main/java/no/nav/dolly/provider/api/OrganisasjonController.java
+++ dolly-backend-app/src/main/java/no/nav/dolly/provider/api/OrganisasjonController.java
@@ -1,6 +1,7 @@
 package no.nav.dolly.provider.api;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import lombok.RequiredArgsConstructor;
 import ma.glasnost.orika.MapperFacade;
 import no.nav.dolly.bestilling.organisasjonforvalter.OrganisasjonClient;
@@ -8,7 +9,6 @@ import no.nav.dolly.domain.jpa.OrganisasjonBestilling;
 import no.nav.dolly.domain.jpa.OrganisasjonBestillingProgress;
 import no.nav.dolly.domain.resultset.RsOrganisasjonBestilling;
 import no.nav.dolly.domain.resultset.entity.bestilling.RsOrganisasjonBestillingStatus;
-import no.nav.dolly.exceptions.NotFoundException;
 import no.nav.dolly.repository.OrganisasjonBestillingProgressRepository;
 import no.nav.dolly.service.OrganisasjonBestillingService;
 import org.springframework.http.HttpStatus;
@@ -20,11 +20,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.HttpClientErrorException;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+import static java.util.Objects.isNull;
 import static java.util.Objects.nonNull;
 
 @RestController
@@ -55,7 +57,7 @@ public class OrganisasjonController {
         Optional<List<OrganisasjonBestillingProgress>> bestillingProgress = bestillingProgressRepository.findByBestillingId(bestillingId);
 
         if (bestillingProgress.isEmpty()) {
-            throw new NotFoundException("Fant ikke noen bestillinger med ID: " + bestillingId);
+            throw new HttpClientErrorException(HttpStatus.BAD_REQUEST, "Fant ikke noen bestillinger med ID: " + bestillingId);
         }
 
         bestillingProgress.ifPresent(progressList ->
@@ -63,15 +65,21 @@ public class OrganisasjonController {
     }
 
     @GetMapping("/bestilling")
-    @Operation(description = "Hent status på bestilling basert på bestillingId eller brukerId")
-    public List<OrganisasjonBestillingProgress> hentStatus(@RequestParam(required = false) Long bestillingId, @RequestParam(required = false) String brukerId) {
+    @Operation(description = "Hent status på bestilling basert på bestillingId eller brukerId. En av de må velges")
+    public List<OrganisasjonBestillingProgress> hentStatus(
+            @Parameter(description = "ID på bestilling av organisasjon", example = "123") @RequestParam(required = false) Long bestillingId,
+            @Parameter(description = "BrukerID som er unik til en Azure bruker (Dolly autensiering)", example = "1k9242uc-638g-1234-5678-7894k0j7lu6n") @RequestParam(required = false) String brukerId) {
+
+        if (isNull(bestillingId) && isNull(brukerId)) {
+            throw new HttpClientErrorException(HttpStatus.BAD_REQUEST, "Kallet må inneholde enten BestillingId eller BrukerID");
+        }
 
         Optional<List<OrganisasjonBestillingProgress>> bestillingProgress = nonNull(brukerId)
                 ? bestillingProgressRepository.findbyBrukerId(brukerId)
                 : bestillingProgressRepository.findByBestillingId(bestillingId);
 
         if (bestillingProgress.isEmpty()) {
-            throw new NotFoundException(nonNull(brukerId)
+            throw new HttpClientErrorException(HttpStatus.BAD_REQUEST, nonNull(brukerId)
                     ? "Fant ikke noen bestillinger med brukerId: " + brukerId
                     : "Fant ikke noen bestillinger med ID: " + bestillingId);
         }
