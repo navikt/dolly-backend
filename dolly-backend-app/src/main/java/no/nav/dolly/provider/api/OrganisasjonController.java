aca1c7296c75ca766177f28eca14d908cf21cd71
stigus <stian.gustavsson@nav.no>
2021-01-15 15:09:37 +0100

    Endret gjenoppretting og litt reformatering

--- dolly-backend-app/src/main/java/no/nav/dolly/provider/api/OrganisasjonController.java
+++ dolly-backend-app/src/main/java/no/nav/dolly/provider/api/OrganisasjonController.java
@@ -13,6 +13,7 @@ import no.nav.dolly.domain.resultset.entity.bestilling.RsOrganisasjonBestillingS
 import no.nav.dolly.service.OrganisasjonBestillingService;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -22,6 +23,9 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Set;
+
+import static java.util.Arrays.asList;
 
 @RestController
 @RequiredArgsConstructor
@@ -43,9 +47,13 @@ public class OrganisasjonController {
         return mapperFacade.map(bestilling, RsOrganisasjonBestillingStatus.class);
     }
 
-    @PutMapping("/gjenopprett")
+    @PutMapping("/gjenopprett/{bestillingId}")
     @Operation(description = "Gjenopprett organisasjon")
-    public DeployResponse gjenopprettOrganisasjon(@RequestBody DeployRequest request) {
+    public DeployResponse gjenopprettOrganisasjon(@PathVariable("bestillingId") Long bestillingId, @RequestParam(value = "miljoer", required = false) String miljoer) {
+
+        DeployRequest request = new DeployRequest(
+                Set.of(bestillingService.fetchBestillingStatusById(bestillingId).getOrganisasjonNummer()),
+                asList(miljoer.split(",")));
 
         return organisasjonClient.gjenopprett(request);
     }
