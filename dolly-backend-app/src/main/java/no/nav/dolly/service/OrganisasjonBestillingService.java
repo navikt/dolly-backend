633156031e69909918655c66750205d21971990e
stigus <stian.gustavsson@nav.no>
2021-01-26 10:33:46 +0100

    Bedre sjekk på ferdig og riktigere navn på test

--- dolly-backend-app/src/main/java/no/nav/dolly/service/OrganisasjonBestillingService.java
+++ dolly-backend-app/src/main/java/no/nav/dolly/service/OrganisasjonBestillingService.java
@@ -27,7 +27,6 @@ import static java.lang.String.format;
 import static java.lang.String.join;
 import static java.time.LocalDateTime.now;
 import static java.util.Collections.singletonList;
-import static java.util.Objects.isNull;
 import static java.util.Objects.nonNull;
 import static no.nav.dolly.util.CurrentAuthentication.getUserId;
 import static org.apache.commons.lang3.BooleanUtils.isTrue;
@@ -63,7 +62,7 @@ public class OrganisasjonBestillingService {
                 .sistOppdatert(bestilling.getSistOppdatert())
                 .organisasjonNummer(bestillingProgress.isEmpty() ? null : bestillingProgress.get(0).getOrganisasjonsnummer())
                 .id(bestillingId)
-                .ferdig(!isNull(bestilling.getFerdig()) && bestilling.getFerdig())
+                .ferdig(isTrue(bestilling.getFerdig()))
                 .feil(bestilling.getFeil())
                 .environments(Arrays.asList(bestilling.getMiljoer().split(",")))
                 .antallLevert(isTrue(bestilling.getFerdig()) && isBlank(bestilling.getFeil()) ? 1 : 0)
@@ -87,7 +86,7 @@ public class OrganisasjonBestillingService {
                             .sistOppdatert(orgBestilling.getSistOppdatert())
                             .organisasjonNummer(bestillingStatus.getOrganisasjonsnummer())
                             .id(bestillingStatus.getBestillingId())
-                            .ferdig(!isNull(orgBestilling.getFerdig()) && orgBestilling.getFerdig())
+                            .ferdig(isTrue(orgBestilling.getFerdig()))
                             .feil(orgBestilling.getFeil())
                             .environments(Arrays.asList(orgBestilling.getMiljoer().split(",")))
                             .antallLevert(isTrue(orgBestilling.getFerdig()) && isBlank(orgBestilling.getFeil()) ? 1 : 0)
