a2375db8975bee1eb51a7bda18a01025b700c97b
kristenhaerum <kristen.herum@nav.no>
2021-02-15 08:00:20 +0100

    Lagt til matr adr på delt bosted

--- dolly-backend-app/src/main/java/no/nav/dolly/bestilling/pdlforvalter/mapper/PdlDeltBostedMappingStrategy.java
+++ dolly-backend-app/src/main/java/no/nav/dolly/bestilling/pdlforvalter/mapper/PdlDeltBostedMappingStrategy.java
@@ -5,13 +5,16 @@ import ma.glasnost.orika.MapperFactory;
 import ma.glasnost.orika.MappingContext;
 import no.nav.dolly.bestilling.pdlforvalter.domain.PdlDeltBosted;
 import no.nav.dolly.bestilling.pdlforvalter.domain.PdlDeltBosted.PdlDelteBosteder;
+import no.nav.dolly.bestilling.pdlforvalter.domain.PdlMatrikkeladresse;
 import no.nav.dolly.bestilling.pdlforvalter.domain.PdlVegadresse;
 import no.nav.dolly.domain.resultset.tpsf.Person;
 import no.nav.dolly.mapper.MappingStrategy;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDate;
+import java.util.Collection;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static no.nav.dolly.domain.CommonKeysAndUtils.CONSUMER;
 import static org.apache.commons.lang3.BooleanUtils.isTrue;
@@ -29,14 +32,30 @@ public class PdlDeltBostedMappingStrategy implements MappingStrategy {
                     public void mapAtoB(Person person, PdlDelteBosteder deltBosted, MappingContext context) {
 
                         deltBosted.getDelteBosteder()
-                                .addAll(person.getBoadresse().stream()
-                                        .filter(boAdresse -> isTrue(boAdresse.getDeltAdresse()))
-                                        .map(boAdresse -> PdlDeltBosted.builder()
-                                                .vegadresse(mapperFacade.map(boAdresse, PdlVegadresse.class))
-                                                .kilde(CONSUMER)
-                                                .startdatoForKontrakt(LocalDate.now())
-                                                .build())
-                                        .collect(Collectors.toList()));
+                                .addAll(
+                                        Stream.of(
+                                                person.getBoadresse().stream()
+                                                        .filter(boAdresse -> isTrue(boAdresse.getDeltAdresse()) &&
+                                                                boAdresse.isGateadresse())
+                                                        .map(boAdresse -> PdlDeltBosted.builder()
+                                                                .vegadresse(mapperFacade.map(boAdresse, PdlVegadresse.class))
+                                                                .kilde(CONSUMER)
+                                                                .startdatoForKontrakt(LocalDate.now())
+                                                                .build())
+                                                        .collect(Collectors.toList()),
+
+                                                person.getBoadresse().stream()
+                                                        .filter(boAdresse -> isTrue(boAdresse.getDeltAdresse()) &&
+                                                                boAdresse.isMatrikkeladresse())
+                                                        .map(boAdresse -> PdlDeltBosted.builder()
+                                                                .matrikkeladresse(mapperFacade.map(boAdresse, PdlMatrikkeladresse.class))
+                                                                .kilde(CONSUMER)
+                                                                .startdatoForKontrakt(LocalDate.now())
+                                                                .build())
+                                                        .collect(Collectors.toList())
+                                        )
+                                                .flatMap(Collection::stream)
+                                                .collect(Collectors.toList()));
                     }
                 })
                 .register();
