eaaa8f685141b971caa5a6d44c132ec2feee7e19
stigus <stian.gustavsson@nav.no>
2021-03-22 08:29:08 +0100

    Query fix

--- dolly-backend-app/src/main/java/no/nav/dolly/repository/IdentRepository.java
+++ dolly-backend-app/src/main/java/no/nav/dolly/repository/IdentRepository.java
@@ -35,7 +35,7 @@ public interface IdentRepository extends CrudRepository<Testident, String> {
             "and bp.ident is not null and length(bp.ident) = 11")
     List<GruppeBestillingIdent> getBestillingerFromGruppe(@Param(value = "gruppe") Testgruppe testgruppe);
 
-    @Query("select distinct ti from Testident ti " +
+    @Query("select ti from Testident ti " +
             "join BestillingProgress bp on bp.ident = ti.ident " +
             "and ti.testgruppe.id = :gruppe " +
             "join Bestilling b on b.id = bp.bestillingId " +
