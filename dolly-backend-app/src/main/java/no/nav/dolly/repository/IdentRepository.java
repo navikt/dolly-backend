90a360036cd94f7d903398a2706a8b18cf83945e
stigus <stian.gustavsson@nav.no>
2021-03-25 10:52:44 +0100

    Gruppering i query

--- dolly-backend-app/src/main/java/no/nav/dolly/repository/IdentRepository.java
+++ dolly-backend-app/src/main/java/no/nav/dolly/repository/IdentRepository.java
@@ -40,6 +40,7 @@ public interface IdentRepository extends CrudRepository<Testident, String> {
             "join BestillingProgress bp on bp.ident = ti.ident " +
             "join Bestilling b on b.id = bp.bestillingId " +
             "where b.gruppe.id = :gruppe_id " +
+            "group by ti.ident, b.sistOppdatert " +
             "order by b.sistOppdatert")
     Page<Testident> getBestillingerFromGruppePaginert(@Param(value = "gruppe_id") Long gruppeId, Pageable pageable);
 
