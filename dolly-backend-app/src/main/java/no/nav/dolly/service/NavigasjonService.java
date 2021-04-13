d186c6af428474ec3a3946a986d8c08d4d88b815
stigus <stian.gustavsson@nav.no>
2021-04-13 14:24:27 +0200

    Returnerer sidetall navigert ident finnes på

--- dolly-backend-app/src/main/java/no/nav/dolly/service/NavigasjonService.java
+++ dolly-backend-app/src/main/java/no/nav/dolly/service/NavigasjonService.java
@@ -1,27 +1,29 @@
 package no.nav.dolly.service;
 
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
-import org.springframework.stereotype.Service;
-
 import lombok.RequiredArgsConstructor;
 import ma.glasnost.orika.MapperFacade;
 import no.nav.dolly.bestilling.tpsf.TpsfService;
 import no.nav.dolly.domain.jpa.Testident;
 import no.nav.dolly.domain.resultset.entity.testgruppe.RsTestgruppe;
+import no.nav.dolly.domain.resultset.entity.testgruppe.RsTestgruppeMedBestillingId;
 import no.nav.dolly.domain.resultset.entity.testident.RsWhereAmI;
 import no.nav.dolly.domain.resultset.tpsf.Person;
 import no.nav.dolly.domain.resultset.tpsf.Relasjon;
 import no.nav.dolly.exceptions.NotFoundException;
 import no.nav.dolly.repository.IdentRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class NavigasjonService {
 
     private final IdentRepository identRepository;
+    private final TestgruppeService testgruppeService;
     private final TpsfService tpsfService;
     private final MapperFacade mapperFacade;
 
@@ -35,11 +37,22 @@ public class NavigasjonService {
 
         List<Testident> identsFound = identRepository.findByIdentIn(identer);
         if (!identsFound.isEmpty()) {
+            RsTestgruppeMedBestillingId testgruppe = testgruppeService.fetchPaginertTestgruppeById(identsFound.get(0).getTestgruppe().getId(), 0, 5000);
+
+            int i = 0;
+            for (RsTestgruppeMedBestillingId.IdentBestilling identBestilling : testgruppe.getIdenter()) {
+                if (identBestilling.getIdent().equals(identsFound.get(0).getIdent())) {
+                    i = testgruppe.getIdenter().indexOf(identBestilling);
+                    break;
+                }
+            }
+
 
             return RsWhereAmI.builder()
                     .gruppe(mapperFacade.map(identsFound.get(0).getTestgruppe(), RsTestgruppe.class))
                     .identHovedperson(identsFound.get(0).getIdent())
                     .identNavigerTil(ident)
+                    .sidetall(Math.floorDiv(i, 10))
                     .build();
         } else {
 
