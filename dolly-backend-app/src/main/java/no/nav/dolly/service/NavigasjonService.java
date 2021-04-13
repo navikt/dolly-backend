a11314b4cb23cdeabc91f33ddb774a1803014360
stigus <stian.gustavsson@nav.no>
2021-04-13 14:55:32 +0200

    Fix

--- dolly-backend-app/src/main/java/no/nav/dolly/service/NavigasjonService.java
+++ dolly-backend-app/src/main/java/no/nav/dolly/service/NavigasjonService.java
@@ -5,16 +5,17 @@ import ma.glasnost.orika.MapperFacade;
 import no.nav.dolly.bestilling.tpsf.TpsfService;
 import no.nav.dolly.domain.jpa.Testident;
 import no.nav.dolly.domain.resultset.entity.testgruppe.RsTestgruppe;
-import no.nav.dolly.domain.resultset.entity.testgruppe.RsTestgruppeMedBestillingId;
 import no.nav.dolly.domain.resultset.entity.testident.RsWhereAmI;
 import no.nav.dolly.domain.resultset.tpsf.Person;
 import no.nav.dolly.domain.resultset.tpsf.Relasjon;
 import no.nav.dolly.exceptions.NotFoundException;
 import no.nav.dolly.repository.IdentRepository;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
 import java.util.List;
+import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -23,7 +24,7 @@ import java.util.stream.Collectors;
 public class NavigasjonService {
 
     private final IdentRepository identRepository;
-    private final TestgruppeService testgruppeService;
+    private final IdentService identService;
     private final TpsfService tpsfService;
     private final MapperFacade mapperFacade;
 
@@ -37,16 +38,10 @@ public class NavigasjonService {
 
         List<Testident> identsFound = identRepository.findByIdentIn(identer);
         if (!identsFound.isEmpty()) {
-            RsTestgruppeMedBestillingId testgruppe = testgruppeService.fetchPaginertTestgruppeById(identsFound.get(0).getTestgruppe().getId(), 0, 5000);
+            Page<Testident> bestillingerFromGruppePaginert = identService.getBestillingerFromGruppePaginert(identsFound.get(0).getTestgruppe().getId(), 0, 5000);
 
-            int i = 0;
-            for (RsTestgruppeMedBestillingId.IdentBestilling identBestilling : testgruppe.getIdenter()) {
-                if (identBestilling.getIdent().equals(identsFound.get(0).getIdent())) {
-                    i = testgruppe.getIdenter().indexOf(identBestilling);
-                    break;
-                }
-            }
-
+            Optional<Testident> first = bestillingerFromGruppePaginert.stream().filter(testident -> testident.getIdent().equals(identsFound.get(0).getIdent())).findFirst();
+            int i = bestillingerFromGruppePaginert.toList().indexOf(first.orElse(null));
 
             return RsWhereAmI.builder()
                     .gruppe(mapperFacade.map(identsFound.get(0).getTestgruppe(), RsTestgruppe.class))
