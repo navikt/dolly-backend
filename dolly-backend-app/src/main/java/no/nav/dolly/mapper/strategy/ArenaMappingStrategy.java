7584c91ec66b7565a4e32dba32f7e6ab82605e79
stigus <stian.gustavsson@nav.no>
2021-05-20 09:40:57 +0200

    Håndterer aktiveringsDato basert på innsendte vedtak fra datoer

--- dolly-backend-app/src/main/java/no/nav/dolly/mapper/strategy/ArenaMappingStrategy.java
+++ dolly-backend-app/src/main/java/no/nav/dolly/mapper/strategy/ArenaMappingStrategy.java
@@ -1,17 +1,24 @@
 package no.nav.dolly.mapper.strategy;
 
-import static java.util.Objects.nonNull;
-import static no.nav.dolly.domain.resultset.arenaforvalter.ArenaBrukertype.UTEN_SERVICEBEHOV;
-import static no.nav.dolly.domain.resultset.arenaforvalter.ArenaKvalifiseringsgruppe.IKVAL;
-
 import ma.glasnost.orika.CustomMapper;
 import ma.glasnost.orika.MapperFactory;
 import ma.glasnost.orika.MappingContext;
 import no.nav.dolly.domain.resultset.arenaforvalter.ArenaBrukerUtenServicebehov;
 import no.nav.dolly.domain.resultset.arenaforvalter.ArenaNyBruker;
 import no.nav.dolly.domain.resultset.arenaforvalter.Arenadata;
+import no.nav.dolly.domain.resultset.arenaforvalter.RsArenaAap;
+import no.nav.dolly.domain.resultset.arenaforvalter.RsArenaAap115;
 import no.nav.dolly.mapper.MappingStrategy;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Objects;
+import java.util.stream.Stream;
+
+import static java.util.Objects.nonNull;
+import static no.nav.dolly.domain.resultset.arenaforvalter.ArenaBrukertype.UTEN_SERVICEBEHOV;
+import static no.nav.dolly.domain.resultset.arenaforvalter.ArenaKvalifiseringsgruppe.IKVAL;
 
 @Component
 public class ArenaMappingStrategy implements MappingStrategy {
@@ -19,7 +26,7 @@ public class ArenaMappingStrategy implements MappingStrategy {
     @Override
     public void register(MapperFactory factory) {
         factory.classMap(Arenadata.class, ArenaNyBruker.class)
-                .customize(new CustomMapper<Arenadata, ArenaNyBruker>() {
+                .customize(new CustomMapper<>() {
                     @Override
                     public void mapAtoB(Arenadata arenadata, ArenaNyBruker arenaNyBruker, MappingContext context) {
 
@@ -30,6 +37,19 @@ public class ArenaMappingStrategy implements MappingStrategy {
                             if (nonNull(arenadata.getInaktiveringDato())) {
                                 arenaNyBruker.getUtenServicebehov().setStansDato(arenadata.getInaktiveringDato().toLocalDate());
                             }
+                        } else if (!arenadata.getAap().isEmpty() || !arenadata.getAap115().isEmpty()) {
+                            arenaNyBruker.setAktiveringsDato(
+                                    Stream.of(
+                                            arenadata.getAap().stream()
+                                                    .filter(Objects::nonNull)
+                                                    .map(RsArenaAap::getFraDato),
+                                            arenadata.getAap115().stream()
+                                                    .filter(Objects::nonNull)
+                                                    .map(RsArenaAap115::getFraDato))
+                                            .flatMap(Stream::distinct)
+                                            .map(LocalDateTime::toLocalDate)
+                                            .min(LocalDate::compareTo)
+                                            .orElse(null));
                         }
                     }
                 })
